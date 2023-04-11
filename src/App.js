58c3a618ecc21ea0b35a1ecf86accf82e7cc8368
Anavuajna <anavuajna@yandex.ru>
2023-04-11 18:43:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState, useLayoutEffect } from 'react';
+import { gsap } from "gsap";
 import './App.css';
+import { TextPlugin } from "gsap/TextPlugin";
+gsap.registerPlugin(TextPlugin);
+
 
 function App() {
+
+  const [boring, setBoring] = useState('');
+
+  const fetchData = async () => {
+    const response = await fetch('http://www.boredapi.com/api/activity/');
+    const data = await response.json();
+    console.log(response, data)
+    setBoring(data.activity)
+  };
+
+  useEffect(() => {
+        fetchData()
+  }, [])
+
+  useLayoutEffect(() => {
+     gsap.context(() => {
+      gsap.to('.heading', {text: 'Не знаешь, чем заняться?', duration: 3.5,  ease: 'power1.inOut'});
+      gsap.from('.boring', {opacity: 0, duration: 2, delay: 2});
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='heading'> </h1>
+      <h2 className='boring'>{boring}</h2>
+      <button onClick={fetchData}>Жми на конпку</button>
     </div>
   );
 }
