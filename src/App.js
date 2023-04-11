09f8d7909faa804bb7d4e8753a9577774840a978
Anavuajna <anavuajna@yandex.ru>
2023-04-11 19:21:10 +0300

    добавила визуал

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useLayoutEffect } from 'react';
 import { gsap } from "gsap";
 import './App.css';
+import video from './movie.mp4';
 import { TextPlugin } from "gsap/TextPlugin";
 gsap.registerPlugin(TextPlugin);
 
@@ -12,7 +13,6 @@ function App() {
   const fetchData = async () => {
     const response = await fetch('http://www.boredapi.com/api/activity/');
     const data = await response.json();
-    console.log(response, data)
     setBoring(data.activity)
   };
 
@@ -22,16 +22,18 @@ function App() {
 
   useLayoutEffect(() => {
      gsap.context(() => {
-      gsap.to('.heading', {text: 'Не знаешь, чем заняться?', duration: 3.5,  ease: 'power1.inOut'});
-      gsap.from('.boring', {opacity: 0, duration: 2, delay: 2});
+      gsap.to('.heading', {text: 'Не знаешь, чем заняться?', duration: 3,  ease: 'power1.inOut'});
+      gsap.from('.boring', {opacity: 0, duration: 2, delay: 3});
+      gsap.from('.btn', {opacity: 0, duration: 2, delay: 1});
     });
   }, []);
 
   return (
     <div className="App">
+      <video src={video} autoPlay muted loop></video>
       <h1 className='heading'> </h1>
       <h2 className='boring'>{boring}</h2>
-      <button onClick={fetchData}>Жми на конпку</button>
+      <button className='btn' onClick={fetchData}>Жми на конпку</button>
     </div>
   );
 }
